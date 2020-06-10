829e98e0247468f8d03210b18fe0215b0b881ce2
Jean - Samuel Eric BOURGUIT <65498199+KaizerShibbyWan@users.noreply.github.com>
2020-06-11 01:25:54 +0200

    Update ipconfig.py

--- p6_module/ipconfig.py
+++ p6_module/ipconfig.py
@@ -24,7 +24,7 @@ def set_ipconfig(var1):
     except IndentationError:
         print('Une ereur de type "IndentationError" a été levée')
 def hostname(var1):
-      """Modification du hostname et du fichier hosts"""
+    """Modification du hostname et du fichier hosts"""
     try:
        os.system("sudo hostname" " " +var1["hostname"])
        fichier = open("/etc/hosts", "a")
@@ -41,5 +41,4 @@ def hostname(var1):
     except ImportError:
         print('Une erreur de type "ImportError" a été levée')
     except IndentationError:
-        print('Une ereur de type "IndentationError" a été levée')
-                                                                              
+        print('Une ereur de type "IndentationError" a été levée')                                                                            
