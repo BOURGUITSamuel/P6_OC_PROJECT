a1a7be9ad515451f787aec69872f1946f3b570b4
Jean - Samuel Eric BOURGUIT <65498199+KaizerShibbyWan@users.noreply.github.com>
2020-06-12 23:24:49 +0200

    Update ipconfig.py

--- p6_module/ipconfig.py
+++ p6_module/ipconfig.py
@@ -5,8 +5,8 @@ def set_ipconfig(var1):
     """Configuration du fichier interfaces de l'OS Debian"""
     try:
       fichier = open("/etc/network/interfaces", "a")
-      fichier.write(var1["type"]+"\n")
       fichier.write(var1["int"]+"\n")
+      fichier.write(var1["type"]+"\n")
       fichier.write(var1["ip"]+"\n")
       fichier.write(var1["mask"]+"\n")
       fichier.write(var1["gaw"]+"\n")
